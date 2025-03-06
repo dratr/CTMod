75d340649750835bfc51b92c64ae8aad7e4ad2af
Dahk Celes <40327147+DDCorkum@users.noreply.github.com>
2025-03-06 13:09:30 +0100

    Merge upstream changes in 1227f1bb9ac1abe48be55d18fca34159296bb241
    - Update for WoW 11.1

--- CT_BarMod/CT_BarMod_SpellFlyout.lua
+++ CT_BarMod/CT_BarMod_SpellFlyout.lua
@@ -21,86 +21,13 @@ local module = select(2, ...);
 
 local spellFlyout = CreateFrame("Frame", "CT_BarMod_SpellFlyout", nil, "SecureFrameTemplate, ResizeLayoutFrame, FlyoutPopupTemplate")
 Mixin(spellFlyout, SpellFlyoutMixin);
-spellFlyout:SetToplevel(true)
-spellFlyout:Hide()
-spellFlyout:SetFrameStrata("DIALOG")
-spellFlyout:SetFrameLevel(10)
 spellFlyout:EnableMouse(true)
-spellFlyout.ignoreInlayout = true  -- sic
+spellFlyout:SetScript("OnShow", spellFlyout.OnShow)
+spellFlyout:SetScript("OnHide", spellFlyout.OnHide)
+spellFlyout:SetScript("OnEvent", spellFlyout.OnEvent)
+spellFlyout:OnLoad()
 
-spellFlyout.Background = CreateFrame("Frame", nil, spellFlyout)
-spellFlyout.Background:SetAllPoints()
-
-spellFlyout.Background.End = spellFlyout.Background:CreateTexture()
-spellFlyout.Background.End:SetAtlas("UI-HUD-ActionBar-IconFrame-FlyoutButton", true)
-
-spellFlyout.Background.HorizontalMiddle = spellFlyout.Background:CreateTexture()
-spellFlyout.Background.HorizontalMiddle:SetAtlas("_UI-HUD-ActionBar-IconFrame-FlyoutMidLeft", true)  -- sic
-
-spellFlyout.Background.VerticalMiddle = spellFlyout.Background:CreateTexture()
-spellFlyout.Background.VerticalMiddle:SetAtlas("!UI-HUD-ActionBar-IconFrame-FlyoutMid", true) -- sic
-
-spellFlyout.Background.Start = spellFlyout.Background:CreateTexture()
-spellFlyout.Background.Start:SetAtlas("UI-HUD-ActionBar-IconFrame-FlyoutBottom", true)
-
-spellFlyout:SetScript("OnShow", SpellFlyout_OnShow)
-spellFlyout:SetScript("OnHide", SpellFlyout_OnHide)
-
-spellFlyout:SetScript("OnEvent", function(self, event, ...)
-	if (event == "SPELL_UPDATE_COOLDOWN") then
-		local i = 1;
-		local button = _G["CT_BarMod_SpellFlyoutButton"..i];
-		while (button and button:IsShown()) do
-			SpellFlyoutButton_UpdateCooldown(button);
-			i = i+1;
-			button = _G["CT_BarMod_SpellFlyoutButton"..i];
-		end
-	elseif (event == "CURRENT_SPELL_CAST_CHANGED") then
-		local i = 1;
-		local button = _G["CT_BarMod_SpellFlyoutButton"..i];
-		while (button and button:IsShown()) do
-			SpellFlyoutButton_UpdateState(button);
-			i = i+1;
-			button = _G["CT_BarMod_SpellFlyoutButton"..i];
-		end
-	elseif (event == "SPELL_UPDATE_USABLE") then
-		local i = 1;
-		local button = _G["CT_BarMod_SpellFlyoutButton"..i];
-		while (button and button:IsShown()) do
-			SpellFlyoutButton_UpdateUsable(button);
-			i = i+1;
-			button = _G["CT_BarMod_SpellFlyoutButton"..i];
-		end
-	elseif (event == "BAG_UPDATE") then
-		local i = 1;
-		local button = _G["CT_BarMod_SpellFlyoutButton"..i];
-		while (button and button:IsShown()) do
-			SpellFlyoutButton_UpdateCount(button);
-			SpellFlyoutButton_UpdateUsable(button);
-			i = i+1;
-			button = _G["CT_BarMod_SpellFlyoutButton"..i];
-		end
-	elseif (event == "SPELL_FLYOUT_UPDATE") then
-		local i = 1;
-		local button = _G["CT_BarMod_SpellFlyoutButton"..i];
-		while (button and button:IsShown()) do
-			SpellFlyoutButton_UpdateCooldown(button);
-			SpellFlyoutButton_UpdateState(button);
-			SpellFlyoutButton_UpdateUsable(button);
-			SpellFlyoutButton_UpdateCount(button);
-			--SpellFlyoutButton_UpdateGlyphState(button);
-			i = i+1;
-			button = _G["CT_BarMod_SpellFlyoutButton"..i];
-		end
-	elseif (event == "PET_STABLE_UPDATE" or event == "PET_STABLE_SHOW") and not InCombatLockdown() then		-- InCombatLockdown() required because this is insecure
-		self:Hide();  -- TODO: Find a way to securely do this during combat
-	--elseif (event == "ACTIONBAR_PAGE_CHANGED") then
-		--self:Hide();  -- Replaced with OnShow SecureHandler that creates a SecureStateDriver with "[nobar:##] hide;"
-	end
-end)
-
-spellFlyout:SetBorderColor(0.5, 0.5, 0.5)
-spellFlyout.Toggle = nop	-- replaced with a secure snippet
+spellFlyout.Toggle = nop -- replaced with secure snippet
 
 -- Workaround; pushing the buttons forward one frame level
 spellFlyout.buttonsFrame = CreateFrame("Frame", nil, spellFlyout)
@@ -145,22 +72,28 @@ spellFlyout:SetAttribute("createdButtons", 0)
 
 function module.createSpellFlyoutButtons(numSlots)	-- must not be called during combat lockdown
 	for i=spellFlyout:GetAttribute("createdButtons")+1, numSlots do
-		local button = CreateFrame("CheckButton", "CT_BarMod_SpellFlyoutButton" .. i, spellFlyout.buttonsFrame, "SecureActionButtonTemplate,SmallActionButtonTemplate")
+		local button
 		
-		button:SetScript("OnEnter", SpellFlyoutButton_SetTooltip)
-		button:SetScript("OnDragStart", SpellFlyoutButton_OnDrag)
-		button:SetScript("OnLeave", function() GameTooltip:Hide() end)
+		if SpellFlyoutPopupButtonMixin then
+			button = CreateFrame("CheckButton", "CT_BarMod_SpellFlyoutButton" .. i, spellFlyout.buttonsFrame, "SpellFlyoutPopupButtonTemplate,SecureActionButtonTemplate")
+			button.popup = spellFlyout
+		else
+			button = CreateFrame("CheckButton", "CT_BarMod_SpellFlyoutButton" .. i, spellFlyout.buttonsFrame, "SecureActionButtonTemplate,SmallActionButtonTemplate")
+			button:SetScript("OnEnter", SpellFlyoutButton_SetTooltip )
+			button:SetScript("OnDragStart", SpellFlyoutButton_OnDrag)
+			button:SetScript("OnLeave", function() GameTooltip:Hide() end)
+		end
+		
 		button:RegisterForClicks("AnyUp", "AnyDown")
-		
 		SecureHandlerWrapScript(button, "PostClick", button, [=[ if down == false then self:GetParent():GetParent():Hide() end ]=], nil)
-		
+
 		SecureHandlerSetFrameRef(spellFlyout, "button"..i, button)
 		button:SetAttribute("slot", i)
 		button:SetAttribute("type", "spell")
 		spellFlyout:SetAttribute("createdButtons", i)
-		
+
 		button.Icon = _G["CT_BarMod_SpellFlyoutButton"..i.."Icon"]
-		
+
 		function button:updateSpellID(spellID)
 			button.spellID = spellID
 			button.Icon:SetTexture(C_Spell.GetSpellTexture(spellID))
